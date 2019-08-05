9ca97598f9872bdb054da85b59e36aa5e402ab67
kevincharley <Administrator@Asiet-Lab.ASIETDomain.com>
2019-08-05 11:48:53 +0530

    dd

---
+++ app.js
@@ -0,0 +1,8 @@
+
+ greet=require("./greetings")
+greet.saybye();
+greet.sayhello();
+
+for(var i in greet.rr){
+    console.log(greet.rr[i]);
+}
---
+++ greetings.js
@@ -0,0 +1,13 @@
+
+exports.sayhello=function(){
+
+    console.log("hello")
+}
+exports.saybye=function(){
+    console.log("bye")
+}
+exports.saybye=function(){
+    console.log("bye")
+}
+exports.rr=['dgdg','gdegd','gdgd','dgdg'];
+  
